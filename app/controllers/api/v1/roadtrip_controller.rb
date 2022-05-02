11477f891c60c730f29d266276abaef634b61ff3
ahenzler <59706432+ahenzler@users.noreply.github.com>
2022-05-01 21:15:49 -0600

    Remove authentication to test theory

--- app/controllers/api/v1/roadtrip_controller.rb
+++ app/controllers/api/v1/roadtrip_controller.rb
@@ -1,11 +1,8 @@
 class Api::V1::RoadtripController < ApplicationController
   def create
-    user = User.find_by(api_key: params[:api_key])
-    if user
+
       road_trip = CoordinatesFacade.get_route(params[:origin], params[:destination])
       render json: RoadtripSerializer.new(road_trip)
-    else
-      render json: { errors: "Invalid API key." }, status: :unauthorized
-    end
+
   end
 end
